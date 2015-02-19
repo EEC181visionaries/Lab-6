91e1fb4253de2e61bfee86a6333c07db92ff5877
MaJessica9 <jamma@ucdavis.edu>
2015-02-18 18:14:54 -0800

    Update start.c

--- start.c
+++ start.c
@@ -7,5 +7,17 @@ int main()
 {
   volatile int * reg = (int *) 0xFF200000;
   
-  int 
+  int start_flag = 0, stop_flag = 0;
+  char input[INPUT_LENGTH];
+  
+  while(1)
+  {
+    printf('Please enter on, off, or check to change or view the status of the camera: ');
+    fgets(input, INPUT_LENGTH, stdin);
+    
+    if (strcmp(input, 'on')||strcmp(input, 'on')||strcmp(input, 'on'))
+    {
+      
+    }
+  }
 }
