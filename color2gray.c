08464236ca6bc5c8af432e4166efa470e0d8d064
MaJessica9 <jamma@ucdavis.edu>
2015-02-25 18:03:15 -0800

    Update color2gray.c

--- color2gray.c
+++ color2gray.c
@@ -1,8 +1,20 @@
+// gray = .21 * red + .72 * green + .07 * blue;
+
 #include <stdio.h>
 #include <string.h>
 
 int main(void)
 {
-  volatile int * reg = (int *) 0x00000000; // contents of the 
+  volatile int * iRead_Data1 = (int *) 0x00000000; // input from read_data1
+  volatile int * iRead_Data2 = (int *) 0x00000000; // input from read_data2
+  volatile int * oRead_Data1 = (int *) 0x00000000; // output to read_data1
+  volatile int * oRead_Data2 = (int *) 0x00000000; // output to read_data2
+  
+  int red, green, blue;
+  
+  // Separate inputs into rgb
+  
+  // Do the multiplication
+  // Store into the output
   return 0;
 }
