7e01ae3bb8aa6a4d4f74a3650d3606da9976485d
MaJessica9 <jamma@ucdavis.edu>
2015-02-25 21:39:12 -0800

    Update color2gray.c

--- color2gray.c
+++ color2gray.c
@@ -10,11 +10,19 @@ int main(void)
   volatile int * oRead_Data1 = (int *) 0x00000000; // output to read_data1
   volatile int * oRead_Data2 = (int *) 0x00000000; // output to read_data2
   
-  int red, green, blue;
+  int red, green, green_h, green_l, blue, gray;
   
   // Separate inputs into rgb
+  red = *(iRead_Data2) & 1023; // red = iRead_Data2[9:0]
+  green = (*(iRead_Data1) & 31744)/32 + (*(iRead_Data2) & 31744)/1024; // green = {iRead_Data1[14:10], iRead_Data1[14:10]}
+  blue = *(iRead_Data1) & 1023;// blue = iRead_Data1[9:0]
   
   // Do the multiplication
+  gray = .21 * red + .72 * green + .07 * blue;
+  
   // Store into the output
+  *(oRead_Data1) = ((gray & 992) * 32) + gray;  //
+  *(oRead_Data2) = ((gray & 31) * 1024) + gray;
+  
   return 0;
 }
