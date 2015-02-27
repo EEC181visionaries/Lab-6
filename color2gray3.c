03d91f2073bff1e499cab52eb909a495564ebf79
MaJessica9 <jamma@ucdavis.edu>
2015-02-26 17:33:28 -0800

    Create color2gray3.c

---
+++ color2gray3.c
@@ -0,0 +1,35 @@
+// color2gray3.c
+// takes rgb image and returns gray image
+
+//#include <stdio.h>
+//#include <string.h>
+
+#define START_CAM_ADDRESS 0xFF200000 // 0x00000000
+
+int main(void)
+{
+  volatile int * start_cam = (int*) START_CAM_ADDRESS;
+  volatile int * iRead_Data1 = (int *) 0x00000000; // input from read_data1 from Sdram_Control
+  volatile int * iRead_Data2 = (int *) 0x00000000; // input from read_data2 from Sdram_Control
+  volatile int * oRead_Data1 = (int *) 0x00000000; // output to read_data1 into VGA_Controller
+  volatile int * oRead_Data2 = (int *) 0x00000000; // output to read_data2 into VGA_Controller
+  
+  int red, green, green_h, green_l, blue, gray;
+  
+  // Camera on
+  *start_cam = 1;
+  
+  // Separate inputs into rgb
+  red = *(iRead_Data2) & 1023; // red = iRead_Data2[9:0]
+  green = (*(iRead_Data1) & 31744)/32 + (*(iRead_Data2) & 31744)/1024; // green = {iRead_Data1[14:10], iRead_Data2[14:10]}
+  blue = *(iRead_Data1) & 1023;// blue = iRead_Data1[9:0]
+  
+  // Do the multiplication
+  gray = .21 * red + .72 * green + .07 * blue;
+  
+  // Store into the output
+  *(oRead_Data1) = ((gray & 992) * 32) + gray;  // {green[9:5], blue[9:0]}
+  *(oRead_Data2) = ((gray & 31) * 1024) + gray; // {green[4:0], red[9:0]}
+  
+  return 0;
+}
